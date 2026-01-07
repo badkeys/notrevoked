00145fefced64f18e66f03f5bf69c50a8fc36360
Hanno Böck <990588+hannob@users.noreply.github.com>
2026-01-07 13:32:25 +0100

    add basic test for expired example certs

---
+++ tests/test_expired.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: 0BSD
+# (c) Hanno Böck
+
+import os
+import pathlib
+import unittest
+
+import isrevoked
+
+TDPATH = f"{os.path.dirname(__file__)}/data/"
+
+
+class TestExpired(unittest.TestCase):
+
+    def test_expired(self):
+        for crtfile in ["expired-crl.crt", "expired-ocsp.crt"]:
+            crtfp = os.path.join(TDPATH, crtfile)
+            crt = pathlib.Path(crtfp).read_bytes()
+            ret = isrevoked.isrevoked(crt)
+            self.assertEqual(ret, "expired")
+
+
+if __name__ == "__main__":
+    unittest.main()
